46255dae61d54ddba6545ad642724f8e14893bbd
jaken92 <jaaakeeen@gmail.com>
2023-03-18 13:09:06 +0100

    changed poss of field

--- Pong/Pong/PlayingField.cs
+++ Pong/Pong/PlayingField.cs
@@ -3,7 +3,7 @@ namespace Pong
 {
 	public class PlayingField
 	{
-		public int width = 80;
+		public int width = 85;
 		public int height = 14;
 
 		public PlayingField()
@@ -12,24 +12,24 @@ namespace Pong
 
 		public void DrawField()
 		{
-			for (int i = 1; i < width; i++)
+			for (int i = 6; i < width; i++)
 			{
                 Console.SetCursorPosition(i, 0);
                 Console.WriteLine('*');
             }
-            for (int i = 1; i < width; i++)
+            for (int i = 6; i < width; i++)
             {
                 Console.SetCursorPosition(i, 13);
                 Console.WriteLine('*');
             }
             for (int i = 0; i < height; i++)
 			{
-                Console.SetCursorPosition(0, i);
+                Console.SetCursorPosition(5, i);
                 Console.WriteLine('*');
             }
 			for (int i = 0; i < height; i++)
 			{
-                Console.SetCursorPosition(80, i);
+                Console.SetCursorPosition(85, i);
                 Console.WriteLine('*');
             }
 		}
